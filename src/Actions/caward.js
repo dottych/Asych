1cce374c07475d7f2c781ade2c801521eb488b65
dottych <96998759+dottych@users.noreply.github.com>
2025-02-28 22:46:38 +0100

    caward fix

---
+++ src/Actions/caward.js
@@ -0,0 +1,14 @@
+module.exports = (params, res) => {
+    // caward stands for Championship Award
+
+    res.end(require('../Utils').response({
+        
+        return: "newpms",
+        i: 0, // invites
+        n: 0, // new PMs count
+        b: 0 // banned
+
+    }));
+
+    // stub, and are we sure this is what it should return?
+}
